3cd4916d6f2137be04bb63036f274b5a6482270e
Septian A Tama <sfirst.tama@gmail.com>
2017-02-16 00:43:47 +0700

    send lists of api endpoints when accessing index route

--- routes/index.js
+++ routes/index.js
@@ -21,24 +21,84 @@ router.get('/', (req, res, next) => {
           "method": "GET"
         },
         "user_details": {
-          "endpoint": "/api/user/:username",
+          "endpoint": "/api/users/:username",
           "method": "GET"
         },
         "edit_profile": {
-          "endpoint": "/api/user/:username",
+          "endpoint": "/api/users/:username",
           "method": "PUT"
         },
         "delete_account": {
-          "endpoint": "/api/user/:username",
+          "endpoint": "/api/users/:username",
           "method": "DELETE"
         },
         "list_of_user_questions" : {
-          "endpoint": "/api/user/:username/questions",
+          "endpoint": "/api/users/:username/questions",
           "method": "GET"
         },
         "list_of_user_answers": {
-          "endpoint": "/api/user/:username/answers",
+          "endpoint": "/api/users/:username/answers",
           "method": "GET"
+        }
+      },
+      "questions" : {
+        "questions_list": {
+          "endpoint": "/api/questions",
+          "method": "GET"
+        },
+        "questions_details": {
+          "endpoint": "/api/questions/:slug",
+          "method": "GET"
+        },
+        "post_a_question": {
+          "endpoint": "/api/questions",
+          "method": "POST"
+        },
+        "edit_a_question": {
+          "endpoint": "/api/questions/:slug",
+          "method": "PUT"
+        },
+        "delete_a_question": {
+          "endpoint": "/api/questions/:slug",
+          "method": "DELETE"
+        },
+        "upvote_question": {
+          "endpoint": "/api/questions/:slug/up",
+          "method": "PUT"
+        },
+        "downvote_question": {
+          "endpoint": "/api/questions/:slug/down",
+          "method": "PUT"
+        },
+        "answers" : {
+          "answer_of_a_question_list": {
+            "endpoint": "/api/questions/:slug/answers",
+            "method": "GET"
+          },
+          "detail_answer_of_a_question_list": {
+            "endpoint": "/api/questions/:slug/answers/:answer_id",
+            "method": "GET"
+          },
+          "post_an_answer": {
+            "endpoint": "/api/questions/:slug/answers",
+            "method": "POST"
+          },
+          "edit_an_answer_of_a_question": {
+            "endpoint": "/api/questions/:slug/answers/:answer_id",
+            "method": "PUT"
+          },
+          "delete_answer_of_a_question": {
+            "endpoint": "/api/questions/:slug/answers/:answer_id",
+            "method": "DELETE"
+          },
+          "upvote_answer": {
+            "endpoint": "/api/questions/:slug/answers/:answer_id/up",
+            "method": "PUT"
+          },
+          "downvote_answer": {
+            "endpoint": "/api/questions/:slug/answers/:answer_id/down",
+            "method": "PUT"
+          }
         }
       }
     }
