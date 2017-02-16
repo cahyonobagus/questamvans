8e16e21a306731a934860b697007799d781c46c0
Septian A Tama <sfirst.tama@gmail.com>
2017-02-16 14:43:41 +0700

    fixing type users to User

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,5 @@
 const passport = require('passport');
-const User = require('../models/users');
+const User = require('../models/User');
 const LocalStrategy = require('passport-local').Strategy;
 const jwt = require('jsonwebtoken');
 require('dotenv').config()
